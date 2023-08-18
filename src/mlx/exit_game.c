96c1128f48268b01e3abd041ed41107ae330f7fb
zinnnn37 <zinnnn37@gmail.com>
2023-08-18 17:20:37 +0900

    destroy_img() && end_game(): should define free_all()

---
+++ src/mlx/exit_game.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_game.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 17:18:42 by minjinki          #+#    #+#             */
+/*   Updated: 2023/08/18 17:20:03 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void	destroy_img(t_game *game)
+{
+	mlx_destroy_image(game->mlx, game->xpm.north);
+	mlx_destroy_image(game->mlx, game->xpm.south);
+	mlx_destroy_image(game->mlx, game->xpm.west);
+	mlx_destroy_image(game->mlx, game->xpm.east);
+}
+
+void	end_game(t_game *game)
+{
+	destroy_img(game);
+	mlx_destroy_window(game->mlx, game->win);
+	//free_all();
+	exit(0);
+}
