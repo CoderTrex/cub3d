2a8d02fb169f2c49b7ae4ee38b3b5ed9f0d20c5a
zinnnn37 <zinnnn37@gmail.com>
2023-08-31 15:01:38 +0900

    rm unused variable

--- include/struct.h
+++ include/struct.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 13:20:39 by minjinki          #+#    #+#             */
-/*   Updated: 2023/08/30 12:32:24 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/08/31 15:00:49 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,8 +138,6 @@ typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
-
-	int		buf[HEIGHT][WIDTH];
 
 	t_img	img;
 	t_map	map;
